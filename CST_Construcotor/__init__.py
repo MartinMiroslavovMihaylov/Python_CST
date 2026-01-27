2397856eccf76c50cc779f2e6df1b731eaac89fc
Martin Mihailov <martinmihailov93@gmail.com>
2026-01-27 13:29:53 +0100

    correction in the __init__.py and .yml file

--- CST_Construcotor/__init__.py
+++ CST_Construcotor/__init__.py
@@ -9,11 +9,11 @@ except PackageNotFoundError:
     __version__ = "0+unknown"  # fallback for source installs
 
 # --- Import classes from CST_Constructor.py ---
-from .CST_Constructor import Constructor, Curves
+from .CST_Constructor import CST_Commands, Curves
 
 # --- Exported names ---
 __all__ = [
     "__version__",
-    "Constructor",
+    "CST_Commands",
     "Curves",
 ]
